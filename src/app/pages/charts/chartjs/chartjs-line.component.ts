77db250204fce7ce2dd7bd6f1e0af210ad8f1b75
khaiwen5177 <khaiwen96@hotmail.com>
2020-07-20 22:45:01 +0800

    UPDATE: display patient1 celcius in real time

--- src/app/pages/charts/chartjs/chartjs-line.component.ts
+++ src/app/pages/charts/chartjs/chartjs-line.component.ts
@@ -5,7 +5,6 @@ import { DatePipe } from '@angular/common';
 //import { Component, OnInit, ViewChild } from '@angular/core';
 import * as firebase from 'firebase/app';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { isUndefined } from 'util';
 
 @Component({
   selector: 'ngx-chartjs-line',
@@ -20,138 +19,63 @@ export class ChartjsLineComponent implements OnDestroy {
   themeSubscription: any;
   dateTime: any;
   timer;
-  tempCount: any = 0;
-  checkSuccess: any = 0;
 
   temp: any = [];
+  tempChild: any = [];
 
   patients: any = [];
-  patients1: any = [];
   dateTimeList: any = [];
 
   temperatureList: any[] = [];
-  temperatureList1: any = [];
-  temperatureList2: any = [];
   tempColor: any;
-  tempColor1: any;
-  tempColor2: any;
 
   constructor(private theme: NbThemeService, public datepipe: DatePipe, public db: AngularFireDatabase) {
+    //loop patient1 from firebase first 
     this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
 
-
-      /*firebase.database().ref().child("rasp-pi12345/" + "temperature").on('value', (shapshot) => {
-        shapshot.forEach((child) => {
-          this.temp.push(
-            {
-              Celcius: child.val().Celcius,
-              Fahrenheit: child.val().Fahrenheit
-            });
-        })
-      })*/
-
-      /*firebase.database().ref().child("rasp-pi12345/").on('value', (shapshot) => {
-        shapshot.forEach((child) => {
-          this.temp.push(
-            {
-              info: child.val(),
-            });
-        })
-      })*/
-
-      /*var rootRef = firebase.database().ref();
-      var urlRef = rootRef.child("Patient1");
-      rootRef.once("value", function (snapshot) {
-
-      });*/
-
-      /*var rootRef = firebase.database().ref();
-      var urlRef = rootRef.child("user1/DAA Notes/URL");
-      urlRef.once("value", function (snapshot) {
-        snapshot.forEach(function (child) {
-          console.log(child.key + ": " + child.val());
-        });
-      });*/
-
-      /*DatabaseReference ref = users.child("GTjrWgpKjoeXUt4JdBJTYP1JkVT2/fnBOM...`");
-      ref.orderByKey().limitToLast(1).addChildEventListener(...*/
-
-    });
-
+      const colors: any = config.variables;
     firebase.database().ref().on('value', (snap) => {
+      var i = 0
       snap.forEach((child) => {
+        if (this.temperatureList[i] === undefined) {
+          this.temperatureList[i] = []
+        }
 
-        //var dbref = firebase.database().ref("rasp-pi12345/" + child.key).limitToLast(1);
-        //console.log(dbref)
-        //console.log(child.key)
-        //console.log(child.key)
-        this.temp.push(child.key)
+        this.temp[i] = child.key
+        this.tempChild[i] = child.val()
+
+
+        const mapped = Object.keys(this.tempChild[i]).map(key => ({ value: this.tempChild[i][key].Celcius }));
+        this.temperatureList[i].push(mapped[mapped.length - 1].value)
+
+        if(mapped[mapped.length - 1].value >= 37.0){
+          this.tempColor = colors.danger;
+        }else{
+          this.tempColor = colors.primary;
+        }
+
+        console.log(this.temperatureList[i])
+        if (this.temperatureList[i].length >= 10) {
+          this.temperatureList[i].shift();
+        }
+        i++;
 
 
 
       });
-      //console.log(this.temp)
-      //console.log("xxx")
+      //update chart
+      this.updateChart();
     });
-
-    //this.updateRecursive();
-    this.getPatientName();
-   
-
+  });
   }
-
-
-
-  getPatientName() {
-
-    //console.log(this.temp)
-    //var i = 0
-
-    //this.temp.forEach((patient_name) => {
-    for(var i = 0; i < this.temp.length ; i++) { 
-      firebase.database().ref(this.temp[i]).orderByKey().limitToLast(1).on('value', (snap) => {
-        snap.forEach((child) => {
-
-          if (this.temperatureList[i] === undefined) {
-            this.temperatureList[i] = []
-            this.temperatureList[i].push(child.val().Cel)
-          } else {
-            this.temperatureList[i].push(child.val().Cel)
-            //console.log(this.temperatureList[i].length)
-            if (this.temperatureList[i].length > 10) {
-              this.temperatureList[i].shift()
-            }
-          }
-
-          
-
-          //i++;
-
-        });
-      });
-    //});
-    }
-
-
-    console.log(this.temperatureList)
-
-      this.updateChart()
-
-  }
-
 
   ngOnDestroy(): void {
     this.themeSubscription.unsubscribe();
     clearTimeout(this.timer);
   }
 
-  genRand(min, max, decimalPlaces) {
-    var rand = Math.random() * (max - min) + min;
-    var power = Math.pow(10, decimalPlaces);
-    return Math.floor(rand * power) / power;
-  }
+  updateChart() {
 
-  updateRecursive() {
     this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
 
       const colors: any = config.variables;
@@ -159,162 +83,38 @@ export class ChartjsLineComponent implements OnDestroy {
 
       this.dateTime = new Date();
 
-      console.log(this.temp)
-      var i = 0
+      //get patient object
+      for (var i = 0; i < this.temp.length; i++) {
 
-      this.temp.forEach((patient_name) => {
-        firebase.database().ref(patient_name).orderByKey().limitToLast(1).on('value', (snap) => {
-          snap.forEach((child) => {
-
-            console.log(patient_name)
-            console.log(i)
-            
-            if (this.temperatureList[i] === undefined) {
-              this.temperatureList[i] = []
-              this.temperatureList[i].push(child.val().Cel)
-            } else {
-              this.temperatureList[i].push(child.val().Cel)
-              if (this.temperatureList[i].length > 10) {
-                this.temperatureList[i].shift()
-              }
-            }
-
-
-
-            /*if (this.temperatureList[this.temperatureList.length - 1] < 37.5) {
-              this.tempColor = colors.primary;
-            } else {
-              this.tempColor = colors.danger;
-            }*/
-            //onsole.log(this.temp[i])
-            
-            this.patients[i] =
-            {
-              data: this.temperatureList[i],
-              label: this.temp[i],
-              backgroundColor: NbColorHelper.hexToRgbA(colors.primary, 0.3),
-              borderColor: colors.primary
-            }
-            //console.log(this.patients[i])
-            i++;
-
-          });
-        });
-      });
-
-      
-
-      this.dateTimeList.push(this.datepipe.transform(this.dateTime, 'hh:mm:ss aa'));
-      if(this.tempCount === 0){
-        this.dateTimeList.shift()
-        this.tempCount = this.tempCount + 1
-      }
-      else{
-        if(this.dateTimeList.length > 10){
-          this.dateTimeList.shift(); 
-        }
-      }
-
-
-      this.information = {
-        labels: this.dateTimeList,
-        datasets: this.patients
-      };
-      
-      //console.log(this.patients)
-      //console.log(this.temperatureList)
-
-      this.options = {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          xAxes: [
-            {
-              gridLines: {
-                display: true,
-                color: chartjs.axisLineColor,
-              },
-              ticks: {
-                fontColor: chartjs.textColor,
-              },
-            },
-          ],
-          yAxes: [
-            {
-              gridLines: {
-                display: true,
-                color: chartjs.axisLineColor,
-              },
-              ticks: {
-                fontColor: chartjs.textColor,
-              },
-            },
-          ],
-        },
-        legend: {
-          labels: {
-            fontColor: chartjs.textColor,
-          },
-        },
-      };
-    });
-
-    this.timer = setTimeout(() => {
-      this.updateRecursive();
-    }, 5000);
-  }
-  
-  updateChart(){
-    
-    this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
-
-      const colors: any = config.variables;
-      const chartjs: any = config.variables.chartjs;
-
-      this.dateTime = new Date();
-
-      //var i = 0
-
-      for(var i = 0; i < this.temp.length; i++ ){
-        //console.log("hi" + i)
         this.patients[i] =
         {
           data: this.temperatureList[i],
           label: this.temp[i],
-          backgroundColor: NbColorHelper.hexToRgbA(colors.primary, 0.3),
-          borderColor: colors.primary
+          backgroundColor: NbColorHelper.hexToRgbA(this.tempColor, 0.3),
+          borderColor: this.tempColor
         }
       }
 
-
+      //get date
       this.dateTimeList.push(this.datepipe.transform(this.dateTime, 'hh:mm:ss aa'));
-      if(this.tempCount === 0){
-        this.dateTimeList.shift()
-        this.tempCount = this.tempCount + 1
-      }
-      else{
-        if(this.dateTimeList.length > 10){
-          this.dateTimeList.shift(); 
-        }
+
+      if (this.dateTimeList.length >= 10) {
+        this.dateTimeList.shift();
       }
 
-
+      console.log(this.patients)
+      //assign patient and date to chart
       this.information = {
         labels: this.dateTimeList,
         datasets: this.patients
       };
       
-      //console.log(this.patients)
       console.log(this.temperatureList)
 
-
     });
-    this.timer = setTimeout(() => {
-      this.getPatientName()
-    }, 5000);
   }
 
- 
+
 
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@
 export const environment = {
   production: true,
 
-  firebase: {
+  /*firebase: {
     apiKey: "AIzaSyAveLds-NKQXtjEklUmnopr2gtsovzUo4o",
     authDomain: "rasp-pi12345.firebaseapp.com",
     databaseURL: "https://rasp-pi12345.firebaseio.com",
@@ -19,5 +19,17 @@ export const environment = {
     storageBucket: "rasp-pi12345.appspot.com",
     messagingSenderId: "454450314706",
     appId: "1:454450314706:web:ef88b66cbb8a616b9f9c0b"
+  }*/
+
+  firebase: {
+    apiKey: "AIzaSyCk9TV47zKtSnk-zVm61CsZ0wzGAPcM7iw",
+    authDomain: "sensorfirebase-f7290.firebaseapp.com",
+    databaseURL: "https://sensorfirebase-f7290.firebaseio.com",
+    projectId: "sensorfirebase-f7290",
+    storageBucket: "sensorfirebase-f7290.appspot.com",
+    messagingSenderId: "197160106010",
+    appId: "1:197160106010:web:4efdff108c18780c98f9a3",
   }
+
+
 };
